155839f9e6425c32421627e2a832bc939f02f20c
Balaji693 <balajivirat693@gmail.com>
2020-01-07 22:05:49 +0530

    unity invokerepeating

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject player;
+
+
+    void Start()
+    {
+        InvokeRepeating("players", 2, 1);
+    }
+
+    void players()
+    {
+        float x = Random.Range(-2.0f, 2.0f);
+        float z = Random.Range(-2.0f, 2.0f);
+        Instantiate(player, new Vector3(x, 2, z), Quaternion.identity);
+    }
+}
